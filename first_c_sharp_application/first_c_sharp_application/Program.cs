731607c963013e65578ead2f0f40ef2d2a0c6858
Michael Aragon <michaeljaragon@gmail.com>
2021-02-02 10:38:03 -0700

    First commit

---
+++ first_c_sharp_application/first_c_sharp_application/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace first_c_sharp_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "12.5";
+            double result = 0;
+            if(double.TryParse(str,System.Globalization.NumberStyles.Float,
+                System.Globalization.NumberFormatInfo.CurrentInfo,out result))
+            {
+                Console.WriteLine(double.TryParse(str, System.Globalization.NumberStyles.Float,
+                System.Globalization.NumberFormatInfo.CurrentInfo, out result).ToString());
+            }
+            
+        }
+    }
+}
