7e95ead281d4da4d02730cd1b8aec3db9b5d6425
Michael Aragon <michaeljaragon@gmail.com>
2021-02-12 16:32:09 -0700

    Added new bank account program

---
+++ MySuperBank/MySuperBank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Michael", 50);
+            Console.WriteLine($"Account {account.Number}" +
+                $" was created for {account.Balance} with {account.Owner}");
+            account.MakeWithdrawal(40, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
--- first_c_sharp_application/first_c_sharp_application/Program.cs
+++ first_c_sharp_application/first_c_sharp_application/Program.cs
@@ -2,18 +2,22 @@
 
 namespace first_c_sharp_application
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string str = "12.5";
-            double result = 0;
-            if(double.TryParse(str,System.Globalization.NumberStyles.Float,
-                System.Globalization.NumberFormatInfo.CurrentInfo,out result))
-            {
-                Console.WriteLine("Hello World!");
-            }
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        string str = "12.5";
+    //        double result = 0;
+    //        if(double.TryParse(str,System.Globalization.NumberStyles.Float,
+    //            System.Globalization.NumberFormatInfo.CurrentInfo,out result))
+    //        {
+    //            Console.WriteLine("Hello World!");
+    //        }
             
-        }
+    //    }
+    //}
+    class Something
+    {
+
     }
 }
